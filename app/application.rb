dcab25b0ae0d41a77103a9800c623f03a600422a
Isaac Villicana <villicana.i@gmail.com>
2018-06-11 23:56:43 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,11 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
       @@cart.each {|item| resp.write "#{item}\n"}
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
